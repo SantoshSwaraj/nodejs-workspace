b7e1640ddaa5fefc119fb9df86e7495e535b2112
santoshGlobal <santoshGlobal@gmail.com>
2019-01-30 19:12:49 +0530

    Process Events Listerns Added

--- 5_Lesson_MVC_Architechture/src/models/connection.js
+++ 5_Lesson_MVC_Architechture/src/models/connection.js
@@ -19,6 +19,24 @@ function getConnection(){
     return _conn;
 }
 
+process.on('SIGINT',function(){
+    console.log("App Termination Due To SIGINT");
+    _conn.close();
+    process.exit(0); //leaving process
+});
+
+process.once('SIGTERM',function(){
+    console.log("App Termination Due To SIGTERM");
+    _conn.close();
+    process.exit(0); //leaving process
+});
+
+process.once('SIGUSR2',function(){
+    console.log("App Termination Due To SIGUSR2 /Userdefined Signal");
+    _conn.close();
+    process.kill(process.pid,'SIGUSR2'); //leaving process
+});
+
 module.exports = {
     open:open,
     getConnection:getConnection
